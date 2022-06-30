603211357fd1664ca44232f5fd0032c40e10c82f
mbugua-s <smbuguaaa@gmail.com>
2022-06-30 15:16:18 +0300

    Styling the registration page

--- project-root/app/Views/user/register.php
+++ project-root/app/Views/user/register.php
@@ -1,22 +1,59 @@
 <?= $this->extend('layouts/main.php') ?>
 
 <?= $this->section('main_content') ?>
-    <h2>Register</h2>
+    <link rel = "stylesheet" href = "/CSS/reg.css">
+<?= $this->endSection() ?>
+
+<?= $this->section('main_content') ?>
+    <h2 class="mb-3 col-4 row justify-content-center">Register</h2>
     <form method = 'POST' action = '/customer/register'>
-        <label>First Name: </label>
+        
+        <div class="row justify-content-center">
+        <div class="mb-3 col-4">
+        <label class="order">First Name: </label>
         <input type = "text" name = "register_firstname">
-        <label>Last Name: </label>
+        </div>
+        </div>
+
+        <div class="row justify-content-center">
+        <div class="mb-3 col-4">
+        <label class="order">Last Name: </label>
         <input type = "text" name = "register_lastname">
-        <label>Email Address: </label>
+        </div>
+        </div>
+
+        <div class="row justify-content-center">
+        <div class="mb-3 col-4">
+        <label class="order">Email Address: </label>
         <input type = "email" name = "register_email">
+        </div>
+        </div>
+
+        <div class="row justify-content-center">
+        <div class="mb-3 col-4">
         <label>Password: </label>
         <input type = "password" name = "register_password">
+        </div>
+        </div>
+
+        <div class="row justify-content-center">
+        <div class="mb-3 col-4">
         <label>Phone Number: </label>
         <input type = "number" name = "register_number">
+        </div>
+
+        <div class="row justify-content-center">
+        <div class="mb-3 col-4">
         <label>Your Address: </label>
         <input type = "text" name = "register_location">
+        </div>
+        </div>
 
-        <input type = "submit" name = "register_submit" value = "REGISTER">
+        <div class="row justify-content-center">
+        <div class="mb-3">
+        <button type = "submit" name = "register_submit" class="btn btn-primary" value = "REGISTER">Register</button>
+        </div>
+        
     </form>
 
 <?= $this->endSection() ?>
