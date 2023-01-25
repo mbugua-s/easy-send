958f295d2366849e7315cbfe89602bd66b0970a9
mbugua-s <smbuguaaa@gmail.com>
2023-01-25 08:03:43 +0000

    Updated project to facilitate hosting

--- project-root/app/Config/App.php
+++ project-root/app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'http://165.232.74.110/';
 
     /**
      * --------------------------------------------------------------------------
